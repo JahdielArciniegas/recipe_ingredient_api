3fefdd4ac9a6fd6f086f4530ba6aadcb953f2171
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2025-12-09 18:54:26 -0400

    fix: disable validation in the unit

--- src/models/ingredientSchema.ts
+++ src/models/ingredientSchema.ts
@@ -4,7 +4,7 @@ export const ingredientSchema = z.object({
   body: z.object({
     name: z.string().min(3, "Name too short"),
     amount: z.number().min(1, "Amount too short"),
-    unit: z.string().min(3, "Unit too short"),
+    unit: z.string(),
     price: z.number("No number"),
   }),
 });
